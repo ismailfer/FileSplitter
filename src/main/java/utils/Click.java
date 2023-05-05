e5145003015a014bc2f536440244cb02d2abdc13
ismailfer <alphadude2009@gmail.com>
2023-05-05 13:11:12 +0100

    bug fix

--- src/main/java/utils/Click.java
+++ src/main/java/utils/Click.java
@@ -2,24 +2,32 @@ package utils;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 
 public class Click {
 
     public static void printUsage() {
-        System.out.println("Usage: \n     Click <interval-seconds | 60>");
+        System.out.println("Usage: \n     Click <interval-seconds | 60> <key code>");
         System.out.println("   Example:   Click 3600");
+        System.out.println("   Example:   Click 10 0xF00B");
         System.out.println("   Example:   Click 3600000 \"\\n\\n\"");
     }
 
     public static void main(String[] args) {
 
         int interval = 60;
+        int keycode = KeyEvent.VK_F12;
 
         try {
 
             if (args.length == 1) {
                 interval = Integer.parseInt(args[0]);
+            }
+            else if (args.length == 2) {
+                interval = Integer.valueOf(args[0]);
+                keycode = Integer.decode(args[1]);
+
             }
 
             Robot robot = new Robot();
@@ -28,12 +36,14 @@ public class Click {
             //robot.mousePress(InputEvent.BUTTON1_MASK);
             //arobot.mouseRelease(InputEvent.BUTTON1_MASK);
 
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd-HH:mm:ss.SSS");
+
             while (true) {
-                System.out.println(Instant.now() + " kkk");
+                System.out.println(formatter.format(System.currentTimeMillis()) + " " + interval + " 0x" + Integer.toHexString(keycode));
 
                 // Simulate a key press
-                robot.keyPress(KeyEvent.VK_F24);
-                robot.keyRelease(KeyEvent.VK_F24);
+                //robot.keyPress(keycode);
+                robot.keyRelease(keycode);
 
                 Thread.sleep(interval * 1000L);
             }
